512357eb308d5a4c13554552058676ed5aaeae85
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2021-03-03 20:28:12 +0100

    formularze

---
+++ app/controllers/contact_messages_controller.rb
@@ -0,0 +1,27 @@
+class ContactMessagesController < ApplicationController
+  def new
+    @contact_message = ContactMessage.new
+  end
+
+  def create
+    unless params[:contact_message][:terms] == "1"
+      flash[:notice] = 'Musisz wyraźić zgodę na przetwarzanie danych'
+    else
+      @contact_message = ContactMessage.new(message_params)
+
+      if @contact_message.save
+        flash[:notice] = 'Wiadomość zapisana'
+      else
+        flash[:notice] = 'Coś poszło nie tak'
+      end
+    end
+
+    redirect_to new_contact_message_path
+  end
+
+  private
+
+  def message_params
+    params.require(:contact_message).permit(:name, :email, :title, :body)
+  end
+end
